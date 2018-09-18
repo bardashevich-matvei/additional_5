1bb669d4d8ffe16548309ca0755dd714797f6b69
Matvei <stormsword0.0@gmail.com>
2018-09-18 20:56:33 +0300

    not final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const sk=[];
+  sk.push(0);
+  sk.push(0);
+  sk.push(0);
+  sk.push(0);
+  sk.push(0);
+  for (i in str){
+    for (j in bracketsConfig){
+      for (k in bracketsConfig[j]){
+        if (str[i]===bracketsConfig[j][k] && k%2===0) sk[j]++;
+        else if (str[i]===bracketsConfig[j][k]) sk[j]--;
+        if (sk[j]<0) return false;
+      }
+    }
+  }
+  for (ii in sk) if (sk[ii]!=0) return false;
+  return true;
 }
